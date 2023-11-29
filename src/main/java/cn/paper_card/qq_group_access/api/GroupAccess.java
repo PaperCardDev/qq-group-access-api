7dd7f2ce28e6e87332716ae44a2e252abf4e680f
Paper99 <1398872296@qq.com>
2023-11-29 22:45:07 +0800

    1.0

---
+++ src/main/java/cn/paper_card/qq_group_access/api/GroupAccess.java
@@ -0,0 +1,20 @@
+package cn.paper_card.qq_group_access.api;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+public interface GroupAccess {
+    boolean hasMember(long qq) throws Exception;
+
+    void setGroupMemberRemark(long qq, @NotNull String remark) throws Exception;
+
+    void sendNormalMessage(@NotNull String message) throws Exception;
+
+    void sendAtMessage(long qq, @NotNull String message) throws Exception;
+
+    void setMute(long qq, int seconds) throws Exception;
+
+    @NotNull List<GroupMember> getAllMembers() throws Exception;
+}
